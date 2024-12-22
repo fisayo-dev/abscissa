d022fdd5ebd4cafce5c76155699926c6349d8a30
fisayo-dev <olufisayobadina@gmail.com>
2024-12-22 21:05:50 +0100

    set up backend middleware

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,15 @@
 import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
 
+dotenv.config()
 const app = express()
 
+// Setting up necessary middleware
+app.use(cors())
+app.use(express.json({ limit: '50mb' }))
+
+// Setting up port
 app.listen(3000, () => {
     console.log('Listening on port 3000')
 })
