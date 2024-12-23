86fc011667a4de3f1926ab332811f1f86d8a1bbe
fisayo-dev <olufisayobadina@gmail.com>
2024-12-23 23:18:43 +0100

    create public pages

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,6 @@
+import Home from "./Public/Home";
+import Login from "./Public/Login";
+import Signup from "./Public/Signup";
+import PageNotFound from "./Public/404";
+
+export { Home, Signup, Login, PageNotFound }
