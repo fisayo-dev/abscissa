f33f66143cdaf13073e8f64d8233f5c3eca8a16d
fisayo-dev <olufisayobadina@gmail.com>
2024-12-23 23:52:01 +0100

    linked public routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,23 @@
-
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import { Home, Signup, Login, PageNotFound } from "./pages";
 function App() {
-
-  return (
-    <>
-
-    </>
-  )
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="*" element={<PageNotFound />} />
+      </>
+    )
+  );
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
