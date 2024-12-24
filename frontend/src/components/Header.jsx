1885e2dd7e96fa57df37c1017a44d5f8229597b3
fisayo-dev <olufisayobadina@gmail.com>
2024-12-24 16:48:08 +0100

    style: bolded heade text in header component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="app-container py-6">
       <div className="flex items-center justify-between">
-        <h2 className="text-2xl font-bold">Abscissa</h2>
+        <Link to='/' className="text-2xl font-extrabold">Abscissa</Link>
         <div className="hidden md:flex items-center gap-3">
           <Link className="hover:text-white">Home</Link>
           <Link className="hover:text-white">Features</Link>
