ea8787c195fa0747a2221725ea718bb4a854a70e
fisayo-dev <olufisayobadina@gmail.com>
2024-12-24 16:30:14 +0100

    worked on landing page

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
 import { PiIcon } from "lucide-react";
+
+import brain_svg from "../assets/brain_svg.png";
+
 const Landing = () => {
   return (
     <div>
       <div className="py-5">
         <div className="app-container">
-          <div className="grid md:flex gap-5 md:justify-between items-center">
+          <div className="grid md:flex md:my-0 my-10 gap-5 md:gap-0 md:justify-between items-center">
             <div className="grid gap-4 md:text-left md:place-items-start place-items-center text-center">
               <h2 className="lg:text-5xl text-4xl capitalize font-bold">
                 Unlock the fun in every equation
@@ -14,27 +17,23 @@ const Landing = () => {
                 Making Math Fun and Engaging—Explore Concepts, Solve Problems,
                 and Unlock Your Potential!
               </p>
-              <div className="flex gap-5 items-center">
+              <div className="flex gap-2 items-center">
                 <Link to="/signup">
-                  <button className="shadow-md px-5 py-3 flex items-center gap-2 rounded-full bg-pink hover-dark-bg-pink">
+                  <button className="shadow-md text-[0.8rem] px-4 py-3 flex gap-2 items-center gap-2 rounded-full bg-pink hover-dark-bg-pink">
                     <PiIcon className="h-6 w-6" />
                     <p>Get Started</p>
                   </button>
                 </Link>
                 <Link to="/signup">
-                  <button className="shadow-md px-5 py-3 flex items-center gap-2 rounded-full hover-dark-bg-blue border border-slate-600">
+                  <button className="shadow-md text-[0.8rem] px-4 py-3 flex gap-2 items-center rounded-full hover-dark-bg-blue border border-slate-600">
                     <PiIcon className="h-6 w-6" />
                     <p>Explore Features</p>
                   </button>
                 </Link>
               </div>
             </div>
-            <div className="grid items-center">
-              {/* <img
-              src={landing_social_media}
-              alt=""
-              className="md:ml-auto mx-auto w-5/6" */}
-              {/* /> */}
+            <div className="grid md:mr-auto md:place-items-end place-items-center mx-auto justify-end items-center">
+              <img src={brain_svg} alt="" className="w-9/12" />
             </div>
           </div>
         </div>
