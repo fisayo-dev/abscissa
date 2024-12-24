12c2e71e2d65e58e9e79529268a510bdbd3bcf2b
fisayo-dev <olufisayobadina@gmail.com>
2024-12-24 15:40:03 +0100

    jsx: created header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
-  return <div className="app-container">Header</div>;
+  return (
+    <div className="app-container py-4">
+      <div className="flex items-center justify-between">
+        <h2 className="text-2xl font-bold">Abscissa</h2>
+        <div className="flex items-center gap-3">
+          <Link className="hover:text-white">Home</Link>
+          <Link className="hover:text-white">Features</Link>
+          <Link className="hover:text-white">Pricing</Link>
+          <Link className="hover:text-white">FAQ</Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
