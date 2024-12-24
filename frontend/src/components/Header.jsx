be0220fee9faf622829a24654c94c72e6a49ae95
fisayo-dev <olufisayobadina@gmail.com>
2024-12-25 00:34:36 +0100

    style: modified header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom";
+import brain_svg from "../assets/brain_svg.png";
+
 
 const Header = () => {
   return (
     <div className="app-container py-6">
       <div className="flex items-center justify-between">
+        <div className="flex items-center gap-2">
+          <img src={brain_svg} alt="" className="h-6 w-6"/>
         <Link to='/' className="text-2xl font-extrabold">Abscissa</Link>
-        <div className="hidden md:flex items-center gap-3">
+        </div>
+        <div className="hidden md:flex items-center gap-6">
           <Link className="hover:text-white">Home</Link>
           <Link className="hover:text-white">Features</Link>
           <Link className="hover:text-white">Pricing</Link>
