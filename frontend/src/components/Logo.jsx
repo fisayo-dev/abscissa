8a8b7a15d58e83d214b8facad329d3e4bc875bfe
fisayo-dev <olufisayobadina@gmail.com>
2024-12-25 12:14:10 +0100

    jsx: made the whole logo element a link to the main app

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -2,12 +2,12 @@ import brain_svg from "../assets/brain_svg.png";
 import { Link } from "react-router-dom";
 const Logo = () => {
   return (
-    <div className="flex items-center gap-2">
+    <Link className="flex items-center gap-2">
       <img src={brain_svg} alt="" className="h-6 w-6" />
-      <Link to="/" className="text-2xl font-extrabold">
+      <h2 to="/" className="text-2xl font-extrabold">
         Abscissa
-      </Link>
-    </div>
+      </h2>
+    </Link>
   );
 };
 
