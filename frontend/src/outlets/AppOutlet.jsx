43fe1e5ca60bd70e5c3a227759c98972009bd1e9
fisayo-dev <olufisayobadina@gmail.com>
2024-12-25 19:04:14 +0100

    style: modified outlet's header styling

--- frontend/src/outlets/AppOutlet.jsx
+++ frontend/src/outlets/AppOutlet.jsx
@@ -7,7 +7,7 @@ const AppOutlet = () => {
   return (
     <div className="w-full flex">
       <Sidebar />
-      <div className="h-full w-full grid gap-8 px-5 py-6 ">
+      <div className="h-full w-full grid gap-8 px-10 py-6 ">
         <div className="flex items-center justify-between gap-2">
           <div className="flex gap-2 items-center">
             <Diamonds className="h-6 w-6" />
