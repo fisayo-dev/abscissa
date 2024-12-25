2c5a5f53d98874805bd13defcb4ca9120273082d
fisayo-dev <olufisayobadina@gmail.com>
2024-12-25 19:30:23 +0100

    style: redesigning whiteboards

--- frontend/src/pages/Private/UserHome.jsx
+++ frontend/src/pages/Private/UserHome.jsx
@@ -12,19 +12,25 @@ const UserHome = () => {
   return (
     <div className="mx-auto 2xl:w-2/3 md:w-3/4 w-full">
       <div className="mt-10">
-        <div className="grid gap-5">
-          <div className="rounded-xl bg-slate-800 p-5">
+        <div className="flex gap-5">
+          <div className="w-3/4 rounded-xl bg-slate-800 p-5">
             <div className="grid gap-4">
-              <h2 className="text-2xl text-center">Recent Whiteboards</h2>
+              <div className="flex py-1 items-center justify-between">
+                <h2 className="text-xl ">Recent Whiteboards</h2>
+              </div>
               <div className="grid gap-2">
                 {sampleWhiteBoards.map((board, index) => (
                   <div
                     key={index}
-                    className="px-4 py-3 rounded-lg bg-slate-600"
+                    className="text-[0.9rem] px-4 py-3 rounded-lg bg-slate-600"
                   >
                     <div className="flex items-center gap-3">
-                      <PencilIcon className="text-slate-300 h-6 w-6" />
-                      <h2 className="w-full">{board.title}</h2>
+                      <PencilIcon className="text-slate-300 w-10 " />
+                      <h2 className=" w-full">
+                        {board.title.length > 23
+                          ? `${board.title.substring(0, 23)}...`
+                          : board.title}
+                      </h2>
                       <p className="text-right w-full">{board.date}</p>
                     </div>
                   </div>
@@ -32,6 +38,7 @@ const UserHome = () => {
               </div>
             </div>
           </div>
+          <div className=" rounded-xl bg-slate-800 p-5"></div>
         </div>
       </div>
     </div>
