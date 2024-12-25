019f7cabccbcc7d18e92d5509f8cfef738544f58
fisayo-dev <olufisayobadina@gmail.com>
2024-12-25 17:45:36 +0100

    create base header for all private route outlet elements

--- frontend/src/outlets/AppOutlet.jsx
+++ frontend/src/outlets/AppOutlet.jsx
@@ -1,11 +1,25 @@
 import { Outlet } from "react-router-dom";
 import { Sidebar } from "../components";
+import { Diamonds, Message } from "iconsax-react";
+import { BellIcon } from "lucide-react";
 
 const AppOutlet = () => {
   return (
     <div className="w-full flex">
       <Sidebar />
-      <Outlet />
+      <div className="h-full w-full grid gap-8 px-5 py-6 ">
+        <div className="flex items-center justify-between gap-2">
+          <div className="flex gap-2 items-center">
+            <Diamonds className="h-6 w-6" />
+            <p>Premimum Plan</p>
+          </div>
+          <div className="flex items-center gap-2 justify-end">
+            <BellIcon className="h-6 w-6" />
+            <Message className="h-6 w-6" />
+          </div>
+        </div>
+        <Outlet />
+      </div>
     </div>
   );
 };
