bdf8cf43ad039073ce71a9a62a925f125566c74b
fisayo-dev <olufisayobadina@gmail.com>
2024-12-25 12:23:00 +0100

    linked header to link home

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -2,9 +2,9 @@ import brain_svg from "../assets/brain_svg.png";
 import { Link } from "react-router-dom";
 const Logo = () => {
   return (
-    <Link className="flex items-center gap-2">
+    <Link to="/home" className="flex items-center gap-2">
       <img src={brain_svg} alt="" className="h-6 w-6" />
-      <h2 to="/" className="text-2xl font-extrabold">
+      <h2 className="text-2xl font-extrabold">
         Abscissa
       </h2>
     </Link>
