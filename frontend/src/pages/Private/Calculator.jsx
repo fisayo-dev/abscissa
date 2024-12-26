e1d6a0d50585c13e1b62d3d493fe0e4f66a40502
fisayo-dev <olufisayobadina@gmail.com>
2024-12-26 10:58:47 +0100

    jsx: creating calculator page

--- frontend/src/pages/Private/Calculator.jsx
+++ frontend/src/pages/Private/Calculator.jsx
@@ -6,7 +6,7 @@ const Calculator = () => {
   const [screen2, setScreen2] = useState("");
   return (
     <div className="w-4/5 md:w-1/3 mx-auto">
-      <div className="mt-4 grid gap-4">
+      <div className="mt-4 grid gap-3">
         <div className="flex items-center text-slate-400 justify-between">
           <div className="flex items-center gap-2 hover:text-slate-100 cursor-pointer">
             <Menu className="h-6 w-6" />
@@ -16,8 +16,10 @@ const Calculator = () => {
             <Clock className="h-6 w-6" />
           </div>
         </div>
-        <div className="h-[80vh] border-[0.12rem] rounded-lg border-slate-500">
-          <div className="grid"></div>
+        <div className="h-[80vh] border-[0.12rem] rounded-lg border-slate-500 overflow-hidden">
+          <div className="grid p-2">
+            <div className="w-full bg-slate-600 h-[14vh] rounded-lg"></div>
+          </div>
         </div>
       </div>
     </div>
