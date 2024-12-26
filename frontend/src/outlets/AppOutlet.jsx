5052ed067b864cf6120aea5b481dc919f1ae6174
fisayo-dev <olufisayobadina@gmail.com>
2024-12-26 01:11:30 +0100

    style: fixing  grid

--- frontend/src/outlets/AppOutlet.jsx
+++ frontend/src/outlets/AppOutlet.jsx
@@ -5,24 +5,25 @@ import { BellIcon } from "lucide-react";
 
 const AppOutlet = () => {
   return (
-    <div className="w-full grid app-grid">
+    <div className="w-[100vw] block md:flex">
       <Sidebar />
-      <div className="h-full w-full grid gap-8 px-4 md:px-8 py-6 ">
-        <div className="fixed">
-          <div className="flex items-center justify-between ">
-            <div className="flex gap-2 items-center">
-              <Diamonds className="h-6 w-6" />
-              <p>Premimum Plan</p>
-            </div>
-            <div className="flex items-center gap-2 justify-end">
-              <BellIcon className="h-6 w-6" />
-              <Message className="h-6 w-6" />
+      <div className="py-5 w-[60vh]">
+        <div className="w-full">
+          <div className="px-4 md:px-6 fixed w-full top-0 py-6 backdrop-blur-sm">
+            <div className="flex justify-between">
+              <div className="flex gap-2 items-center">
+                <Diamonds className="h-6 w-6" />
+                <p>Premimum Plan</p>
+              </div>
+              <div className="flex items-center gap-2 justify-end">
+                <BellIcon className="h-6 w-6" />
+                <Message className="h-6 w-6" />
+              </div>
             </div>
           </div>
-        </div>
-
-        <div className="mt-10">
-          <Outlet />
+          <div className="px-4 md:px-8 mt-20">
+            <Outlet />
+          </div>
         </div>
       </div>
     </div>
