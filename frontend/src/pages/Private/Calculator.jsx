c0ad06c0516e923612efae047b9b387298f7adfa
fisayo-dev <olufisayobadina@gmail.com>
2024-12-27 21:26:32 +0100

    converted standard btns to react element

--- frontend/src/pages/Private/Calculator.jsx
+++ frontend/src/pages/Private/Calculator.jsx
@@ -16,6 +16,10 @@ const Calculator = () => {
   const [smallScreen, setSmallScreen] = useState("");
   const [largeScreen, setLargeScreen] = useState("0");
 
+  const calculatorBtnStyle =
+    "p-3 rounded-lg text-center cursor-pointer hover:bg-slate-600 bg-slate-700 flex justify-center";
+
+  // Calculator Btn Functions
   const handleDeleteScreen = () => {
     if (smallScreen.toString().trim() !== "") {
       setSmallScreen("");
@@ -49,20 +53,148 @@ const Calculator = () => {
 
   const handleSquareRoot = () => {
     const squareRootValue = Math.sqrt(largeScreen);
-    setSmallScreen(`sqrt(${largeScreen})`)
-    setLargeScreen(squareRootValue)
+    setSmallScreen(`sqrt(${largeScreen})`);
+    setLargeScreen(squareRootValue);
   };
   const handleSquare = () => {
-    const squareValue = Math.pow(largeScreen,2);
-    setSmallScreen(`sqr(${largeScreen})`)
-    setLargeScreen(squareValue)
+    const squareValue = Math.pow(largeScreen, 2);
+    setSmallScreen(`sqr(${largeScreen})`);
+    setLargeScreen(squareValue);
   };
 
   const handleEqualTo = () => {
-    const evaluatedAnswer = eval(largeScreen)
-    setSmallScreen(largeScreen)
-    setLargeScreen(evaluatedAnswer)
-  }
+    const evaluatedAnswer = eval(largeScreen);
+    setSmallScreen(largeScreen);
+    setLargeScreen(evaluatedAnswer);
+  };
+
+  const StandardBtn = () => {
+    return (
+      <>
+        <div
+          className={calculatorBtnStyle}
+          onClick={() => handleInputToScreen("%")}
+        >
+          %
+        </div>
+        <div className={calculatorBtnStyle} onClick={() => clearScreen()}>
+          <p>C</p>
+        </div>
+        <div
+          className={calculatorBtnStyle}
+          onClick={() => handleDeleteScreen()}
+        >
+          <XCircleIcon className="h-6 w-6" />
+        </div>
+        <div className={calculatorBtnStyle} onClick={() => handleSquare()}>
+          <div className="relative flex justify-center gap-0">
+            <p>x</p>
+            <p>2</p>
+          </div>
+        </div>
+        <div className={calculatorBtnStyle} onClick={() => handleSquareRoot()}>
+          <p>sqrt</p>
+        </div>
+        <div
+          className={calculatorBtnStyle}
+          onClick={() => handleInputToScreen("/")}
+        >
+          <DivideIcon className="h-6 w-6" />
+        </div>
+        <div
+          className={calculatorBtnStyle}
+          onClick={() => handleInputToScreen("1")}
+        >
+          <p>1</p>
+        </div>
+        <div
+          className={calculatorBtnStyle}
+          onClick={() => handleInputToScreen("2")}
+        >
+          <p>2</p>
+        </div>
+        <div
+          className={calculatorBtnStyle}
+          onClick={() => handleInputToScreen("*")}
+        >
+          <X />
+        </div>
+        <div
+          className={calculatorBtnStyle}
+          onClick={() => handleInputToScreen("3")}
+        >
+          <p>3</p>
+        </div>
+        <div
+          className={calculatorBtnStyle}
+          onClick={() => handleInputToScreen("4")}
+        >
+          <p>4</p>
+        </div>
+        <div
+          className={calculatorBtnStyle}
+          onClick={() => handleInputToScreen("+")}
+        >
+          <Plus />
+        </div>
+        <div
+          className={calculatorBtnStyle}
+          onClick={() => handleInputToScreen("5")}
+        >
+          <p>5</p>
+        </div>
+        <div
+          className={calculatorBtnStyle}
+          onClick={() => handleInputToScreen("6")}
+        >
+          <p>6</p>
+        </div>
+        <div
+          className={calculatorBtnStyle}
+          onClick={() => handleInputToScreen("-")}
+        >
+          <Minus />
+        </div>
+        <div
+          className={calculatorBtnStyle}
+          onClick={() => handleInputToScreen("7")}
+        >
+          <p>7</p>
+        </div>
+        <div
+          className={calculatorBtnStyle}
+          onClick={() => handleInputToScreen("8")}
+        >
+          <p>8</p>
+        </div>
+        <div
+          className={calculatorBtnStyle}
+          onClick={() => handleInputToScreen("9")}
+        >
+          <p>9</p>
+        </div>
+        <div
+          className={calculatorBtnStyle}
+          onClick={() => handleInputToScreen("0")}
+        >
+          <p>0</p>
+        </div>
+        <div
+          className={calculatorBtnStyle}
+          onClick={() => handleInputToScreen(".")}
+        >
+          <DotIcon />
+        </div>
+        <div
+          className="p-3 rounded-lg text-center cursor-pointer hover-dark-bg-pink bg-pink flex justify-center"
+          onClick={() => handleEqualTo()}
+        >
+          <Equal />
+        </div>
+      </>
+    );
+  };
+
   return (
     <div className="w-full md:w-[380px] mx-auto">
       <div className="mt-4 grid gap-3">
@@ -93,126 +225,7 @@ const Calculator = () => {
               />
             </div>
             <div className="grid gap-2 grid-cols-3">
-              <div
-                className="p-3 rounded-lg text-center cursor-pointer hover:bg-slate-600 bg-slate-700 flex justify-center"
-                onClick={() => handleInputToScreen("%")}
-              >
-                %
-              </div>
-              <div
-                className="p-3 rounded-lg text-center cursor-pointer hover:bg-slate-600 bg-slate-700 flex justify-center"
-                onClick={() => clearScreen()}
-              >
-                <p>C</p>
-              </div>
-              <div
-                className="p-3 rounded-lg text-center cursor-pointer hover:bg-slate-600 bg-slate-700 flex justify-center"
-                onClick={() => handleDeleteScreen()}
-              >
-                <XCircleIcon className="h-6 w-6" />
-              </div>
-              <div className="p-3 rounded-lg text-center cursor-pointer hover:bg-slate-600 bg-slate-700 flex justify-center" onClick={() => handleSquare()}>
-                <div className="relative flex justify-center gap-0">
-                  <p>x</p>
-                  <p>2</p>
-                </div>
-              </div>
-              <div className="p-3 rounded-lg text-center cursor-pointer hover:bg-slate-600 bg-slate-700 flex justify-center" onClick={() => handleSquareRoot()}>
-                <p>sqrt</p>
-              </div>
-              <div
-                className="p-3 rounded-lg text-center cursor-pointer hover:bg-slate-600 bg-slate-700 flex justify-center"
-                onClick={() => handleInputToScreen("/")}
-              >
-                <DivideIcon className="h-6 w-6" />
-              </div>
-              <div
-                className="p-3 rounded-lg text-center cursor-pointer hover:bg-slate-600 bg-slate-700 flex justify-center"
-                onClick={() => handleInputToScreen("1")}
-              >
-                <p>1</p>
-              </div>
-              <div
-                className="p-3 rounded-lg text-center cursor-pointer hover:bg-slate-600 bg-slate-700 flex justify-center"
-                onClick={() => handleInputToScreen("2")}
-              >
-                <p>2</p>
-              </div>
-              <div
-                className="p-3 rounded-lg text-center cursor-pointer hover:bg-slate-600 bg-slate-700 flex justify-center"
-                onClick={() => handleInputToScreen("*")}
-              >
-                <X />
-              </div>
-              <div
-                className="p-3 rounded-lg text-center cursor-pointer hover:bg-slate-600 bg-slate-700 flex justify-center"
-                onClick={() => handleInputToScreen("3")}
-              >
-                <p>3</p>
-              </div>
-              <div
-                className="p-3 rounded-lg text-center cursor-pointer hover:bg-slate-600 bg-slate-700 flex justify-center"
-                onClick={() => handleInputToScreen("4")}
-              >
-                <p>4</p>
-              </div>
-              <div
-                className="p-3 rounded-lg text-center cursor-pointer hover:bg-slate-600 bg-slate-700 flex justify-center"
-                onClick={() => handleInputToScreen("+")}
-              >
-                <Plus />
-              </div>
-              <div
-                className="p-3 rounded-lg text-center cursor-pointer hover:bg-slate-600 bg-slate-700 flex justify-center"
-                onClick={() => handleInputToScreen("5")}
-              >
-                <p>5</p>
-              </div>
-              <div
-                className="p-3 rounded-lg text-center cursor-pointer hover:bg-slate-600 bg-slate-700 flex justify-center"
-                onClick={() => handleInputToScreen("6")}
-              >
-                <p>6</p>
-              </div>
-              <div
-                className="p-3 rounded-lg text-center cursor-pointer hover:bg-slate-600 bg-slate-700 flex justify-center"
-                onClick={() => handleInputToScreen("-")}
-              >
-                <Minus />
-              </div>
-              <div
-                className="p-3 rounded-lg text-center cursor-pointer hover:bg-slate-600 bg-slate-700 flex justify-center"
-                onClick={() => handleInputToScreen("7")}
-              >
-                <p>7</p>
-              </div>
-              <div
-                className="p-3 rounded-lg text-center cursor-pointer hover:bg-slate-600 bg-slate-700 flex justify-center"
-                onClick={() => handleInputToScreen("8")}
-              >
-                <p>8</p>
-              </div>
-              <div
-                className="p-3 rounded-lg text-center cursor-pointer hover:bg-slate-600 bg-slate-700 flex justify-center"
-                onClick={() => handleInputToScreen("9")}
-              >
-                <p>9</p>
-              </div>
-              <div
-                className="p-3 rounded-lg text-center cursor-pointer hover:bg-slate-600 bg-slate-700 flex justify-center"
-                onClick={() => handleInputToScreen("0")}
-              >
-                <p>0</p>
-              </div>
-              <div
-                className="p-3 rounded-lg text-center cursor-pointer hover:bg-slate-600 bg-slate-700 flex justify-center"
-                onClick={() => handleInputToScreen(".")}
-              >
-                <DotIcon />
-              </div>
-              <div className="p-3 rounded-lg text-center cursor-pointer hover-dark-bg-pink bg-pink flex justify-center" onClick={() => handleEqualTo()}>
-                <Equal />
-              </div>
+              <StandardBtn />
             </div>
           </div>
         </div>
