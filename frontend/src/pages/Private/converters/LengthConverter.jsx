ba34870d723822def68c0ad5cfdcc9ce256ee6ca
fisayo-dev <olufisayobadina@gmail.com>
2024-12-28 13:00:47 +0100

    built length converter layout

--- frontend/src/pages/Private/converters/LengthConverter.jsx
+++ frontend/src/pages/Private/converters/LengthConverter.jsx
@@ -1,10 +1,75 @@
+import { useState } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
 const LengthConverter = () => {
+  const [convertFrom, setConvertFrom] = useState("metres");
+  const [convertTo, setConvertTo] = useState("centimetres");
+
+  const units = [
+    "centimetres",
+    "metres",
+    "kilometres",
+    "angstroms",
+    "inches",
+    "feet",
+    "yards",
+    "miles",
+  ];
   return (
     <div className="w-full md:w-4/5 2xl:w-3/5 mx-auto">
       <div className="mt-8">
-        {/* Left side converter */}
-
-        {/* Right side converter */}
+        <div className="grid md:grid-cols-2 grid-col-1 md:gap-5 gap-10">
+          {/* Left side converter */}
+          <div className="grid gap-3">
+            <p className="text-sm text-slate-500">From</p>
+            <input
+              type="number"
+              className="px-3 text-lg border rounded-lg border-slate-600 py-4"
+              placeholder="00"
+            />
+            <Select
+              value={convertFrom}
+              onValueChange={(value) => setConvertFrom(value)}
+            >
+              <SelectTrigger>
+                <SelectValue placeholder="Pick calculator type"></SelectValue>
+              </SelectTrigger>
+              <SelectContent>
+                {units.map((unit) => (
+                  <SelectItem value={unit}>{unit}</SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
+          {/* Right side converter */}
+          <div className="grid gap-3">
+            <p className="text-sm text-slate-500">To</p>
+            <input
+              type="number"
+              className="px-3 text-lg border rounded-lg border-slate-600 py-4"
+              placeholder="00"
+            />
+            <Select
+              value={convertTo}
+              onValueChange={(value) => setConvertTo(value)}
+            >
+              <SelectTrigger>
+                <SelectValue placeholder="Pick calculator type"></SelectValue>
+              </SelectTrigger>
+              <SelectContent>
+                {units.map((unit) => (
+                  <SelectItem value={unit}>{unit}</SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
       </div>
     </div>
   );
