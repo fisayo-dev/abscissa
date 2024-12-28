aa37d3299c6417e4ed109eb7e9dffb045c2161fc
fisayo-dev <olufisayobadina@gmail.com>
2024-12-28 15:35:58 +0100

    building temperature converter

--- frontend/src/pages/Private/converters/TemperatureConverter.jsx
+++ frontend/src/pages/Private/converters/TemperatureConverter.jsx
@@ -10,14 +10,25 @@ import { BackBtn } from "./LengthConverter";
 import { Thermometer } from "lucide-react";
 
 const TemperatureConverter = () => {
-  const units = [
-    "fahrenheit",
-    "kelvin",
-    "celsius",
-  ];
-  const [convertFrom, setConvertFrom] = useState(units[0]);
-  const [convertTo, setConvertTo] = useState(units[2]);
+  const units = ["fahrenheit", "kelvin", "celsius"];
+  const [valueA, setValueA] = useState("");
+  const [valueB, setValueB] = useState("");
+  const [unitA, setUnitA] = useState(units[0]);
+  const [unitB, setUnitB] = useState(units[2]);
 
+  const convertDegree = (side) => {
+    if (unitA == "fahrenheit" && unitB == "celsius") {
+      if (side == "A") {
+        // Convert from F to C
+        const celsiusExpression = (valueA * 5) / 9 - 32;
+        setValueB(celsiusExpression);
+      } else if (side == "B") {
+        // Convert from C to F
+        const fahrenheitExpression = (valueB + 32);
+        setValueA((1.8 * fahrenheitExpression));
+      }
+    }
+  };
   return (
     <div className="w-full md:w-4/5 2xl:w-3/5 mx-auto">
       <div className="py-8 grid gap-6">
@@ -36,17 +47,21 @@ const TemperatureConverter = () => {
               type="number"
               className="px-3 text-lg border rounded-lg border-slate-600 py-4"
               placeholder="Degree"
+              value={valueA}
+              onChange={(e) => {
+                setValueA(e.target.value);
+                convertDegree("A");
+              }}
             />
-            <Select
-              value={convertFrom}
-              onValueChange={(value) => setConvertFrom(value)}
-            >
+            <Select value={unitA} onValueChange={(value) => setUnitA(value)}>
               <SelectTrigger>
                 <SelectValue placeholder="Pick calculator type"></SelectValue>
               </SelectTrigger>
               <SelectContent>
-                {units.map((unit) => (
-                  <SelectItem value={unit}>{unit}</SelectItem>
+                {units.map((unit, index) => (
+                  <SelectItem key={index} value={unit}>
+                    {unit}
+                  </SelectItem>
                 ))}
               </SelectContent>
             </Select>
@@ -58,17 +73,21 @@ const TemperatureConverter = () => {
               type="number"
               className="px-3 text-lg border rounded-lg border-slate-600 py-4"
               placeholder="Degree"
+              value={valueB}
+              onChange={(e) => {
+                setValueB(e.target.value);
+                convertDegree("B");
+              }}
             />
-            <Select
-              value={convertTo}
-              onValueChange={(value) => setConvertTo(value)}
-            >
+            <Select value={unitB} onValueChange={(value) => setUnitB(value)}>
               <SelectTrigger>
                 <SelectValue placeholder="Pick calculator type"></SelectValue>
               </SelectTrigger>
               <SelectContent>
-                {units.map((unit) => (
-                  <SelectItem value={unit}>{unit}</SelectItem>
+                {units.map((unit, index) => (
+                  <SelectItem key={index} value={unit}>
+                    {unit}
+                  </SelectItem>
                 ))}
               </SelectContent>
             </Select>
