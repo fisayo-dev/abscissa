4593f87d2aee882c0597987e9aaf867bfc9541e9
fisayo-dev <olufisayobadina@gmail.com>
2024-12-28 13:33:46 +0100

    create volume converter file

--- frontend/src/pages/Private/converters/VolumeConverter.jsx
+++ frontend/src/pages/Private/converters/VolumeConverter.jsx
@@ -1,7 +1,85 @@
-const VolumeConverter = () => {
-  return (
-    <div>VolumeConverter</div>
-  )
-}
+import { useState } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { BackBtn } from "./LengthConverter";
+import { Cuboid } from "lucide-react";
 
-export default VolumeConverter
+const VolumeConverter = () => {
+  const [convertFrom, setConvertFrom] = useState("milliliters");
+  const [convertTo, setConvertTo] = useState("cubic meters");
+
+  const units = [
+    "milliliters",
+    "cubic centimetres",
+    "litres",
+    "cubic meters",
+    "cubic inches",
+    "cubic feet",
+  ];
+  return (
+    <div className="w-full md:w-4/5 2xl:w-3/5 mx-auto">
+      <div className="py-8 grid gap-6">
+        <div className="flex items-center justify-between">
+          <BackBtn />
+          <div className="flex items-center gap-2">
+            <Cuboid />
+            <p>Volume Converter</p>
+          </div>
+        </div>
+        <div className="grid md:grid-cols-2 grid-col-1 md:gap-5 gap-10">
+          {/* Left side converter */}
+          <div className="grid gap-3">
+            <p className="text-sm text-slate-500">From</p>
+            <input
+              type="number"
+              className="px-3 text-lg border rounded-lg border-slate-600 py-4"
+              placeholder="00"
+            />
+            <Select
+              value={convertFrom}
+              onValueChange={(value) => setConvertFrom(value)}
+            >
+              <SelectTrigger>
+                <SelectValue placeholder="Pick calculator type"></SelectValue>
+              </SelectTrigger>
+              <SelectContent>
+                {units.map((unit) => (
+                  <SelectItem value={unit}>{unit}</SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
+          {/* Right side converter */}
+          <div className="grid gap-3">
+            <p className="text-sm text-slate-500">To</p>
+            <input
+              type="number"
+              className="px-3 text-lg border rounded-lg border-slate-600 py-4"
+              placeholder="00"
+            />
+            <Select
+              value={convertTo}
+              onValueChange={(value) => setConvertTo(value)}
+            >
+              <SelectTrigger>
+                <SelectValue placeholder="Pick calculator type"></SelectValue>
+              </SelectTrigger>
+              <SelectContent>
+                {units.map((unit) => (
+                  <SelectItem value={unit}>{unit}</SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VolumeConverter;
