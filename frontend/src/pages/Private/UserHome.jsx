af3f9a4b9de459f308ac46ce85f86ffc5f0a6d7f
fisayo-dev <olufisayobadina@gmail.com>
2024-12-28 21:22:29 +0100

    modified user dashboard look

--- frontend/src/pages/Private/UserHome.jsx
+++ frontend/src/pages/Private/UserHome.jsx
@@ -1,4 +1,4 @@
-import { BarChartBig, PencilIcon } from "lucide-react";
+import { BarChartBig, Flame, PencilIcon } from "lucide-react";
 import { Link } from "react-router-dom";
 
 const UserHome = () => {
@@ -11,46 +11,42 @@ const UserHome = () => {
   ];
 
   return (
-    <div className="mx-auto 2xl:w-2/3 w-11/12">
-      <div className="mt-10">
-        <div className="grid md:flex gap-5">
-          <div className="w-full md:w-3/4 rounded-xl bg-slate-800 p-5">
+    <div className="mx-auto 2xl:w-2/3 w-full">
+      <div className="my-10">
+        <div className="grid md:flex items-start gap-5">
+          <div className="w-full md:w-3/4 rounded-xl bg-slate-900 p-5">
             <div className="grid gap-4">
               <div className="flex py-1 items-center justify-between">
                 <h2 className="text-xl ">Recent Whiteboards</h2>
-                <Link className="text-slate-300 hover:underline text-sm">
+                <Link className="hidden md:block text-slate-300 hover:underline text-sm">
                   View all
                 </Link>
               </div>
-              <div className="grid gap-2">
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
                 {sampleWhiteBoards.map((board, index) => (
-                  <div
-                    key={index}
-                    className="text-[0.9rem] px-4 py-3 rounded-lg hover:bg-slate-600 bg-slate-700 cursor-pointer"
-                  >
-                    <div className="flex items-center gap-3">
-                      <PencilIcon className="text-slate-300 w-10 " />
-                      <h2 className=" w-full">
-                        {board.title.length > 23
-                          ? `${board.title.substring(0, 23)}...`
-                          : board.title}
-                      </h2>
-                      <p className="text-right w-full">{board.date}</p>
+                  <Link key={index} className="grid gap-2">
+                    <div className="h-48 md:h-32 bg-slate-700  hover:bg-slate-500 rounded-lg" />
+                    <div className="grid gap-1 ">
+                      <h2>{board.title}</h2>
+                      <p className="text-sm">{board.date}</p>
                     </div>
-                  </div>
+                  </Link>
                 ))}
               </div>
             </div>
           </div>
-          <div className="w-full md:w-3/6 overflow-hidden rounded-xl bg-slate-800 p-5">
+          <div className="w-full md:w-3/6 overflow-hidden rounded-xl bg-slate-900 p-5">
             <div className="grid p4">
               <div className="flex py-1 items-center justify-between">
-                <h2 className="text-xl ">Daily Calculations</h2>
+                <h2 className="text-xl ">Daily Streak</h2>
+                <h2 className="">12</h2>
               </div>
               <div className="flex flex-col place-items-center py-10">
-                <BarChartBig className="h-52 w-52"/>
+                <Flame className="h-52 w-52" />
               </div>
-                <Link className="text-right text-slate-300 hover:underline text-sm">View analysis</Link>
+              <Link className="text-right text-slate-300 hover:underline text-sm">
+                View analysis
+              </Link>
             </div>
           </div>
         </div>
