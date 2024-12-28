a91d94099615dd3ea0940eceddab8246ed4c8287
fisayo-dev <olufisayobadina@gmail.com>
2024-12-28 12:34:46 +0100

    created converters elements

--- frontend/src/pages/Private/Converter.jsx
+++ frontend/src/pages/Private/Converter.jsx
@@ -1,7 +1,72 @@
-const Converter = () => {
-  return (
-    <div>Converter</div>
-  )
-}
+import { Ruler, Speedometer, Weight } from "iconsax-react";
+import {
+  Coins,
+  Cuboid,
+  Flame,
+  Scale3DIcon,
+  ThermometerIcon,
+} from "lucide-react";
+import { Link } from "react-router-dom";
 
-export default Converter
+const Converter = () => {
+  const converters = [
+    {
+      text: "Area",
+      icon: <Scale3DIcon className="md:h-12 md:w-12 h-8 w-8" />,
+      link: '/converter/area',
+    },
+    {
+      text: "Length",
+      icon: <Ruler className="md:h-12 md:w-12 h-8 w-8" />,
+      link: '/converter/length',
+    },
+    {
+      text: "Money",
+      icon: <Coins className="md:h-12 md:w-12 h-8 w-8" />,
+      link: '/converter/money',
+    },
+    {
+      text: "Volume",
+      icon: <Cuboid className="md:h-12 md:w-12 h-8 w-8" />,
+      link: '/converter/volume',
+    },
+    {
+      text: "Speed",
+      icon: <Speedometer className="md:h-12 md:w-12 h-8 w-8" />,
+      link: '/converter/speed',
+    },
+    {
+      text: "Energy",
+      icon: <Flame className="md:h-12 md:w-12 h-8 w-8" />,
+      link: '/converter/energy',
+    },
+    {
+      text: "Temperature",
+      icon: <ThermometerIcon className="md:h-12 md:w-12 h-8 w-8" />,
+      link: '/converter/temperature',
+    },
+    {
+      text: "Weight and Mass",
+      icon: <Weight className="md:h-12 md:w-12 h-8 w-8" />,
+      link: '/converter/weight-and-mass',
+    },
+  ];
+  return (
+    <div className="w-full md:w-4/5 2xl:w-3/5 mx-auto">
+      <div className="mt-8">
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-5">
+          {converters.map((converter, index) => (
+            <Link to={converter.link} key={index} className="cursor-pointer hover:bg-slate-700 bg-slate-800 p-4 rounded-lg">
+              <div className="flex items-center justify-center gap-5 flex-col">
+                {converter.icon}
+                <p className="text-center">{converter.text}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Converter;
--- frontend/src/pages/Private/LengthConverter.jsx
+++ frontend/src/pages/Private/LengthConverter.jsx
@@ -1,7 +1,13 @@
 const LengthConverter = () => {
   return (
-    <div>LengthConverter</div>
-  )
-}
+    <div className="w-full md:w-4/5 2xl:w-3/5 mx-auto">
+      <div className="mt-8">
+        {/* Left side converter */}
 
-export default LengthConverter
+        {/* Right side converter */}
+      </div>
+    </div>
+  );
+};
+
+export default LengthConverter;
