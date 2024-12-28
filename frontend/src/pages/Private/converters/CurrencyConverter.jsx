c7e827ff14dfd28b7fa899ca7b4234d45af4ea39
fisayo-dev <olufisayobadina@gmail.com>
2024-12-28 14:01:46 +0100

    create curency converter layout

--- frontend/src/pages/Private/converters/CurrencyConverter.jsx
+++ frontend/src/pages/Private/converters/CurrencyConverter.jsx
@@ -1,7 +1,78 @@
-const CurrencyConverter = () => {
-  return (
-    <div>CurrencyConverter</div>
-  )
-}
+import { useState } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { BackBtn } from "./LengthConverter";
+import { Wallet1 } from "iconsax-react";
 
-export default CurrencyConverter
+const CurrencyConverter = () => {
+  const units = ["dollars", "naira", "cedis", "pounds", "yen"];
+  const [convertFrom, setConvertFrom] = useState(units[0]);
+  const [convertTo, setConvertTo] = useState(units[2]);
+
+  return (
+    <div className="w-full md:w-4/5 2xl:w-3/5 mx-auto">
+      <div className="py-8 grid gap-6">
+        <div className="flex items-center justify-between">
+          <BackBtn />
+          <div className="flex items-center gap-2">
+            <Wallet1 />
+            <p>Currency Converter</p>
+          </div>
+        </div>
+        <div className="grid md:grid-cols-2 grid-col-1 md:gap-5 gap-10">
+          {/* Left side converter */}
+          <div className="grid gap-3">
+            <p className="text-sm text-slate-500">From</p>
+            <input
+              type="number"
+              className="px-3 text-lg border rounded-lg border-slate-600 py-4"
+              placeholder="00"
+            />
+            <Select
+              value={convertFrom}
+              onValueChange={(value) => setConvertFrom(value)}
+            >
+              <SelectTrigger>
+                <SelectValue placeholder="Pick calculator type"></SelectValue>
+              </SelectTrigger>
+              <SelectContent>
+                {units.map((unit) => (
+                  <SelectItem value={unit}>{unit}</SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
+          {/* Right side converter */}
+          <div className="grid gap-3">
+            <p className="text-sm text-slate-500">To</p>
+            <input
+              type="number"
+              className="px-3 text-lg border rounded-lg border-slate-600 py-4"
+              placeholder="00"
+            />
+            <Select
+              value={convertTo}
+              onValueChange={(value) => setConvertTo(value)}
+            >
+              <SelectTrigger>
+                <SelectValue placeholder="Pick calculator type"></SelectValue>
+              </SelectTrigger>
+              <SelectContent>
+                {units.map((unit) => (
+                  <SelectItem value={unit}>{unit}</SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CurrencyConverter;
