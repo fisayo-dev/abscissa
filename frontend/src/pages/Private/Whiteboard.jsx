1f03e2014072c1606e0fcc13a42d48754dbf16a3
fisayo-dev <olufisayobadina@gmail.com>
2024-12-28 22:03:02 +0100

    work on search section layout

--- frontend/src/pages/Private/Whiteboard.jsx
+++ frontend/src/pages/Private/Whiteboard.jsx
@@ -1,11 +1,11 @@
-import { Add } from "iconsax-react";
-import { Search } from "lucide-react";
+import { Add, Grid1, Grid2 } from "iconsax-react";
+import { AppWindowMacIcon, List, Search } from "lucide-react";
 
 const Whiteboard = () => {
   return (
     <div className="mx-auto 2xl:w-2/3 w-full">
       <div className="my-10">
-        <div className="grid gap-5">
+        <div className="grid gap-4">
           <div className="flex items-center justify-between">
             <h2 className="text-xl">Whiteboard</h2>
             <div className="flex items-center gap-3">
@@ -21,6 +21,22 @@ const Whiteboard = () => {
                   <p>Create</p>
                 </div>
               </button>
+            </div>
+          </div>
+          <div className="flex items-center gap-4">
+            <div className="bg-slate-700 px-4 py-3 rounded-full w-full">
+              <div className="flex items-center gap-4">
+                <Search className="h-6 w-6 text-slate-400" />
+                <input type="text" className="w-full" placeholder="Search value"/>
+              </div>
+            </div>
+            <div className="flex items-center gap-2">
+              <div className="p-3 rounded-full cursor-pointer hover:bg-slate-700">
+                <Grid1 className="h-6 w-6"/>
+              </div>
+              <div className="p-3 rounded-full cursor-pointer hover:bg-slate-700">
+                <List className="h-6 w-6"/>
+              </div>
             </div>
           </div>
         </div>
