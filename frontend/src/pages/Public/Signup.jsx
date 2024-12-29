57e2f29c3238aada9b31827d728dfd4b4d6b7bd2
fisayo-dev <olufisayobadina@gmail.com>
2024-12-29 17:54:57 +0100

    modified signup layout

--- frontend/src/pages/Public/Signup.jsx
+++ frontend/src/pages/Public/Signup.jsx
@@ -3,6 +3,7 @@ import { Logo } from "../../components";
 import { Rocket, Users } from "lucide-react";
 import { useState } from "react";
 import Google from "../../assets/Google.svg";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -25,7 +26,9 @@ const Signup = () => {
               <div className="grid gap-3">
                 <div className="flex gap-4 items-center">
                   <div className="grid gap-3">
-                    <label className="font-bold text-slate-300">First Name</label>
+                    <label className="font-bold text-slate-300">
+                      First Name
+                    </label>
                     <div className="border border-slate-600 rounded-full p-4 ">
                       <div className="flex items-center gap-2">
                         <User className="h-6 w-6" />
@@ -38,7 +41,9 @@ const Signup = () => {
                     </div>
                   </div>
                   <div className="grid gap-3">
-                    <label className="font-bold text-slate-300">Last Name</label>
+                    <label className="font-bold text-slate-300">
+                      Last Name
+                    </label>
                     <div className="border border-slate-600 rounded-full p-4 ">
                       <div className="flex items-center gap-2">
                         <Users className="h-6 w-6" />
@@ -53,7 +58,9 @@ const Signup = () => {
                 </div>
 
                 <div className="grid gap-3">
-                  <label className="font-bold text-slate-300">Email Address</label>
+                  <label className="font-bold text-slate-300">
+                    Email Address
+                  </label>
                   <div className="border border-slate-600 rounded-full p-4 ">
                     <div className="flex items-center gap-2">
                       <User className="h-6 w-6" />
@@ -86,15 +93,19 @@ const Signup = () => {
                   </div>
                 </div>
               </div>
-              <div className="grid md:flex justify-center gap-3">
+              <div className="flex justify-center gap-3">
                 <button className="shadow-md text-[0.9rem] px-4 py-3 flex items-center justify-center gap-2 rounded-full bg-pink hover-dark-bg-pink">
                   <Rocket className="h-4 w-4 md:h-6 md:w-6" />
                   <p className="capitalize">Create account</p>
                 </button>
                 <button className="shadow-md text-[0.9rem]  px-4 py-3 font-bold flex items-center justify-center gap-2 rounded-full bg-slate-200 text-slate-900 hover:bg-slate-300">
                   <img src={Google} width={20} height={20} />
-                  <p className="capitalize">Continue with google</p>
+                  {/* <p className="capitalize">Continue with google</p> */}
                 </button>
+              </div>
+              <div className="text-sm text-center">
+                I already have an account?
+                <Link to="/login" className="color-pink hover:underline"> Signup</Link>
               </div>
             </form>
           </div>
