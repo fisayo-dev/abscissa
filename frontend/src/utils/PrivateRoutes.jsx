fa59f35296e27c969c84af6c1472bfdc3ad73653
fisayo-dev <olufisayobadina@gmail.com>
2024-12-31 16:13:58 +0100

    setup private routes

---
+++ frontend/src/utils/PrivateRoutes.jsx
@@ -0,0 +1,10 @@
+import { useNavigate, Outlet } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
+
+const PrivateRoutes = () => {
+  const navigate = useNavigate()
+  const { user } = useAuth();
+  return user ? <Outlet /> : navigate('/login');
+};
+
+export default PrivateRoutes;
