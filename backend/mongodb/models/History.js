7cf1966509af9db4796fd0f0d088963d11840692
fisayo-dev <olufisayobadina@gmail.com>
2024-12-31 12:54:14 +0100

    create history model

---
+++ backend/mongodb/models/History.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose"
+
+const HistorySchema = new mongoose.Schema({
+    date: {type: Date, required: true},
+    expression: {type: String, required: true},
+    result: {type: String, required: true},
+    calculator_type: {type: String, required: true},
+})
+
+const HistoryModel = mongoose.model('History', HistorySchema)
+export default HistoryModel
