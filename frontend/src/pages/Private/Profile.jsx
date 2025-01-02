b6fd27823ca81bbf0b06f4a35919fce0626e492f
fisayo-dev <olufisayobadina@gmail.com>
2025-01-02 16:20:19 +0100

    implemented shadcn button component

--- frontend/src/pages/Private/Profile.jsx
+++ frontend/src/pages/Private/Profile.jsx
@@ -2,11 +2,24 @@ import { useAuth } from "@/contexts/AuthContext";
 import { GraduationCap, Pencil } from "lucide-react";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 const Profile = () => {
   const [userDetails, setUserDetails] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [editProfile, setEditProfile] = useState(true);
   const { user, logout } = useAuth();
   const navigate = useNavigate();
 
@@ -32,7 +45,10 @@ const Profile = () => {
           const data = await response.json();
 
           // Check if the error message indicates token expiration
-          if (data.message && data.message.toLowerCase().includes("token expired")) {
+          if (
+            data.message &&
+            data.message.toLowerCase().includes("token expired")
+          ) {
             alert("Your session has expired. Please log in again.");
             logout();
             navigate("/login");
@@ -73,7 +89,7 @@ const Profile = () => {
         <div className="grid gap-10">
           <div className="relative bg-slate-900 rounded-lg p-5">
             <div className="absolute bottom-4 right-4">
-              <ActionButton />
+              <ActionButton userDetails={userDetails} />
             </div>
 
             <div className="md:flex grid justify-center md:justify-normal items-center gap-5">
@@ -103,11 +119,49 @@ const Profile = () => {
   );
 };
 
-const ActionButton = () => (
-  <div className="flex items-center cursor-pointer bg-slate-800 justify-center hover:bg-slate-700 px-3 py-2 text-sm rounded-full gap-2">
-    <Pencil className="h-5 w-6" />
-    <p className="hidden md:block">Edit Profile</p>
-  </div>
+const ActionButton = ({ userDetails }) => (
+  <Dialog>
+    <DialogTrigger asChild>
+      <div className="flex items-center cursor-pointer bg-slate-800 justify-center hover:bg-slate-700 px-3 py-2 text-sm rounded-full gap-2">
+        <Pencil className="h-5 w-6" />
+        <p className="hidden md:block">Edit Profile</p>
+      </div>
+    </DialogTrigger>
+    <DialogContent>
+      <DialogHeader className="text">
+        <DialogTitle>Edit Profile</DialogTitle>
+        <DialogDescription>
+          <p className="capitalize">Decide how you appear to others</p>
+        </DialogDescription>
+      </DialogHeader>
+      <form action="" className="grid gap-4">
+        <div className="grid gap-2">
+          <label className="font-bold">First Name</label>
+          <Input
+            placeholder="Place in your updated first name"
+            value={userDetails.first_name}
+          />
+        </div>
+        <div className="grid gap-2">
+          <label className="font-bold">Last Name</label>
+          <Input
+            placeholder="Place in your updated first name"
+            value={userDetails.last_name}
+          />
+        </div>
+        <div className="grid gap-2">
+          <label className="font-bold">Education Grade</label>
+          <Input
+            placeholder="Place in your updated first name"
+            value={userDetails.education_grade}
+          />
+        </div>
+        <div className="flex mx-auto">
+          <Button>Save changes</Button>
+        </div>
+      </form>
+    </DialogContent>
+  </Dialog>
 );
 
 export default Profile;
